38f0348cd1cc6aa7f61cfd7af78ad423c09a25b0
Sascha Eglau <se@saschaeglau.com>
2021-08-21 10:58:34 +0200

    add remaining warm up solutions

---
+++ hackerrank/interview-preparation-kit/warm-up-challenges/repeated-string-kotlin/Solution.kt
@@ -0,0 +1,44 @@
+// https://www.hackerrank.com/challenges/repeated-string/problem
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+fun repeatedString(s: String, n: Long): Long {
+    val timesPerString = s.count{ it == 'a'}
+    val partialRepeat = (n % s.length).toInt()
+    return n / s.length * timesPerString + s.subSequence(0, partialRepeat).count {it == 'a'}
+}
+
+fun main(args: Array<String>) {
+    val s = readLine()!!
+
+    val n = readLine()!!.trim().toLong()
+
+    val result = repeatedString(s, n)
+
+    println(result)
+}
